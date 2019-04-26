a707a5f8e2ad96558369de3c4b4edc1d15b673f8
Thomas Schildhauer <tschildhauer@serva-ts.com>
2019-04-26 17:53:48 +0200

    Setup Project

---
+++ Test.Autofac.PluginBase/Program.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Autofac.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Autofac.PluginBase.Autofac;
+using Test.Autofac.PluginBase.Interfaces;
+
+namespace Test.Autofac.PluginBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var registration = new PluginInstantiation();
+            var scope = registration.CreateScope();
+
+            using (scope)
+            {
+                registration.Plugins.Values.ToList().ForEach(p => p.OnStartup());
+                Console.ReadKey();
+            }
+        }
+    }
+}
